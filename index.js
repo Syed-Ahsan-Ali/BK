69d82ce1dc158972fcd0122a150553f0fe72672e
ahsan.ali <ahsan.ali@itboost.com>
2017-09-09 23:01:36 +0500

    Initial commit

---
+++ index.js
@@ -0,0 +1,37 @@
+const express=require("express");
+var Connection=require("tedious").Connection;
+var request=require("tedious").Request;
+var config={
+  userName:"sa",
+  password:"123",
+  server:"DESKTOP-81ANGD2\\SQLEXPRESS",
+  options: {
+    instanceName: 'MSSQLSERVER',
+    database: 'Proposal',  //the username above should have granted permissions in order to access this DB.
+    debug: {
+      packet: false,
+      payload: false,
+      token: false,
+      data: false
+    },
+    //encrypt: true
+  }
+};
+var connection=new Connection(config);
+connection.on("connect",function (err) {
+  if(err){
+    console.log(err);
+  }
+  else{
+    console.log("success");
+  }
+});
+const app=express();
+
+app.get("/",function (req,res) {
+  var query=connection.query();
+  res.send(query);
+});
+app.listen(3000,function () {
+  console.log("App is listening at port 3000");
+});
