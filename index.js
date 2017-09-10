bae312975a9fc78224b2a1900d3a850a67b0292b
ahsan.ali <ahsan.ali@itboost.com>
2017-09-10 20:06:33 +0500

    changes

--- index.js
+++ index.js
@@ -1,39 +1,71 @@
 const express=require("express");
+var Tags=require("./Models/Tags");
 var Connection=require("tedious").Connection;
-var request=require("tedious").Request;
 var config={
   userName:"sa",
   password:"123",
   server:"localhost",
-  // database: 'Proposal',
-  // instanceName: 'SQLEXPRESS',
-  // port:1433
-  // // options: {
-  //   instanceName: 'MSSQLSERVER',
-  //   database: 'Proposal',  //the username above should have granted permissions in order to access this DB.
-  //   debug: {
-  //     packet: false,
-  //     payload: false,
-  //     token: false,
-  //     data: false
-  //   },
-  //   //encrypt: true
-  // }
+  options:{
+    instanceName:"SQLEXPRESS",
+    database:"Proposal"
+  }
 };
-var connection=new Connection(config);
-connection.on("connect",function (err) {
+
+var Request=require("tedious").Request;
+function getTags(callback){
+  var connection=new Connection(config);
+  var newdata=[];
+  connection.on("connect",function (err) {
+    if(err) {
+      console.log("Not Connected"+err);
+    }else {
+      var request=new Request("select *from tags;",function (err,rowCount) {
+        if(err)
+        {
+          callback(err);
+        }
+        else
+        {
+          if(rowCount<1)
+          {
+            callback(null,false);
+          }
+          else{
+            callback(null,newdata);
+          }
+        }
+      });
+      process.setMaxListeners(0);
+      request.on("row", function (columns) {
+        newdata.push({Id:columns[0].value,screenName:columns[1].value});
+        //console.log(JSON.stringify(columns));
+        // columns.forEach(function (column) {
+        //   //console.log(JSON.stringify(column));
+        //   newdata.push({val:column.value});
+        // });
+      });
+      request.on("done",function (rowCount,more) {
+        console.log(rowCount+"rows Returned")
+      });
+      connection.execSql(request);
+    }
+  });
+
+
+}
+const app=express();
+app.get("/tags",function (req,res) {
+  getTags(function(err,rows){
   if(err){
     console.log(err);
   }
+  else if(rows){
+    res.send(rows);  }
   else{
-    console.log("success");
+    console.log("No Data Available");
   }
-});
-const app=express();
+  });
 
-app.get("/",function (req,res) {
-  //var query=connection.query();
-  res.send("Sample Responce");
 });
 app.listen(3000,function () {
   console.log("App is listening at port 3000");
